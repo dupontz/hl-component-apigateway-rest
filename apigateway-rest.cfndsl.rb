733a292bb9b0a67269ce7464cd0b28b391c9758e
Gustavo Dupont Rodrigues Gonçalves <gustavo.dupont@gmail.com>
2025-01-29 16:05:23 -0800

    Update apigateway-rest.cfndsl.rb
    ts no edge certificate

--- apigateway-rest.cfndsl.rb
+++ apigateway-rest.cfndsl.rb
@@ -26,7 +26,7 @@ CloudFormation do
 
   ApiGateway_DomainName(:CustomDomain) {
     Condition(:HasDomainName)
-    CertificateArn FnIf('HasEdgeCertificateArn', Ref(:EdgeCertificateArn), Ref('AWS::NoValue'))
+    # CertificateArn FnIf('HasEdgeCertificateArn', Ref(:EdgeCertificateArn), Ref('AWS::NoValue'))
     DomainName FnSub("#{custom_dns_prefix}.${EnvironmentName}.${DnsDomain}")
     EndpointConfiguration {
       Types endpoint_configuration['types']
